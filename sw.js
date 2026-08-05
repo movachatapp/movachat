3e919212b12dbb3663285274cd1f761c6459c42c
movachatapp <movachatapp@gmail.com>
2026-08-05 13:53:14 -0400

    Rev5.2

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // 📱 SERVICE WORKER MOVACHAT (Versión Corregida)
 // ========================================================
 
-const CACHE_NAME = 'movachat-v5.1';
+const CACHE_NAME = 'movachat-v5.2';
 
 // Archivos básicos para guardar en memoria del dispositivo
 const ASSETS_TO_CACHE = [
